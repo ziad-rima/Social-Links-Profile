7b0f7efa34024837a6a587bb5f309cd4aa219343
Ziad Rima <ziadrima49@gmail.com>
2025-01-08 20:10:58 +0100

    fix photo not uploading | fix URLs

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- [Solution URL](https://your-solution-url.com)
+- [Live site URL](https://sociallinksfrontend.netlify.app/)
 
 ## My process
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <article className="header">
-            <img className="header-image" src="../assets/images/avatar-jessica.jpeg" alt="a person's picture" />
+            <img className="header-image" src="\assets\images\avatar-jessica.jpeg" alt="a person's picture" />
             <h1 className="header-name">Jessica Randall</h1>
             <p className="header-location">London, United Kingdom</p>
             <p className="header-description">"Front-end developer and avid reader."</p>
